8f41b8055cf73f1b13f3c0c067cdbec33dc5c4c1
Mario Gomes <markkennedy232323@gmail.com>
2023-05-18 23:05:48 +0100

    adicionando auth

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Mail\ExampleMain;
-use App\Mail\Welcome;
-use App\Models\User;
-use Illuminate\Support\Facades\Mail;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,16 +13,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get("/teste",function(){
 
-    //return (new ExampleMain([]))->render();
-
-    $user = User::find(1);
-
-    Mail::to("teste@email")
-    ->send(new Welcome);
-    return redirect("/");
-});
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
